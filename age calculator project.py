c326e326bfde84412ec7dcb429f2d5c7b3280e55
Srinivasa siva sagar Nagamboina <120013821+sagarnagamboina@users.noreply.github.com>
2022-12-09 23:17:53 +0530

    Add files via upload

---
+++ age calculator project.py
@@ -0,0 +1,19 @@
+# import datetime
+# s=input("Enter DOB:")
+# a=s.split("/")
+# D1=int(a[0])
+# M1=int(a[1])
+# Y1=int(a[-1])
+# d=input("Enter Today Date:" )
+# f=d.split("/")
+# D2,M2,Y2=int(f[0]),int(f[1]),int(f[-1])
+# today=datetime.date(Y2,M2,D2)
+# DOB=datetime.date(Y1,M1,D1)
+# sd=abs(DOB-today)
+# Days=sd.days
+# print("Days survived :",Days)   
+
+r=int(input())
+def triangle():
+    for i in range(ord(65),ord(91)):
+        for j in range(int(ord(65)),int(ord()))
